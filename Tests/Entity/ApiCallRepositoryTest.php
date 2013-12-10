02b8edbbd748fdc3566a0d8a7c46fff96e0a2ce4
Patrick Ruckstuhl <patrick@ch.tario.org>
2013-12-11 00:37:57 +0100

    db based on migrations

--- Tests/Entity/ApiCallRepositoryTest.php
+++ Tests/Entity/ApiCallRepositoryTest.php
@@ -16,8 +16,13 @@ class ApiCallRepositoryTest extends WebTestCase
     public function setUp()
     {
         $this->runCommand('doctrine:database:create', array('--connection' => 'eveapi'));
-        $this->runCommand('doctrine:schema:create', array('--em' => 'eveapi'));
+	$this->runCommand('doctrine:migrations:migrate', array('--em' => 'eveapi', '--no-interaction' => true));
         $entityManager = $this->getContainer()->get('doctrine.orm.eveapi_entity_manager');
         $this->repository = $entityManager->getRepository('TariochEveapiFetcherBundle:ApiCall');
+    }
+
+    public function tearDown()
+    {
+	$this->runCommand('doctrine:database:drop', array('--connection' => 'eveapi', '--no-interaction' => true, '--force' => true));
     }
 }
