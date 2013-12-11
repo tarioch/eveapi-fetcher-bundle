9cf5fda25b2dc15151702f556ec7033578803127
Patrick Ruckstuhl <patrick@tario.org>
2013-12-11 23:14:39 +0100

    refactored EveWorker and added test for it

---
+++ Component/Worker/ApiUpdater.php
@@ -0,0 +1,150 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Component\Worker;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use Tarioch\PhealBundle\DependencyInjection\PhealFactory;
+use Doctrine\ORM\EntityManager;
+use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
+use Pheal\Exceptions\PhealException;
+use Symfony\Component\DependencyInjection\Container;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Stopwatch\Stopwatch;
+
+/**
+ * @DI\Service("tarioch.eveapi.worker.apiupdater")
+ */
+class ApiUpdater
+{
+    const ERROR_MAX = 5;
+
+    private $logger;
+    private $phealFactory;
+    private $entityManager;
+    private $container;
+
+    /**
+     * @DI\InjectParams({
+     * "phealFactory" = @DI\Inject("tarioch.pheal.factory"),
+     * "entityManager" = @DI\Inject("doctrine.orm.eveapi_entity_manager"),
+     * "container" = @DI\Inject("service_container"),
+     * "logger" = @DI\Inject("logger")
+     * })
+     */
+    public function __construct(
+        PhealFactory $phealFactory,
+        EntityManager $entityManager,
+        Container $container,
+        LoggerInterface $logger
+    ) {
+        $this->phealFactory = $phealFactory;
+        $this->entityManager = $entityManager;
+        $this->container = $container;
+        $this->logger = $logger;
+    }
+
+    public function update($apiCallId)
+    {
+        $call = $this->entityManager->find('TariochEveapiFetcherBundle:ApiCall', $apiCallId);
+        if ($this->isCallStillValid($call)) {
+            $api = $call->getApi();
+            $apiInfo = $api->getSection() . ' ' . $api->getName();
+
+            $stopwatch = new Stopwatch();
+            $stopwatch->start($apiInfo);
+            try {
+                $cachedUntil = $this->updateApi($call);
+                $call->clearErrorCount();
+                $call->setCachedUntil(new \DateTime($cachedUntil));
+            } catch (PhealException $e) {
+                $this->logger->error($apiCallId . ': Api call failed: ' . $e->getMessage());
+                $call->increaseErrorCount();
+                if ($call->getErrorCount() > self::ERROR_MAX) {
+                    $call->setDisabled(true);
+                }
+            }
+
+            $minutesToEarliestNextCall = $call->getApi()->getCallInterval() * (1 + $call->getErrorCount());
+            $earliestNextCall = new \DateTime('now', new \DateTimeZone('UTC'));
+            $earliestNextCall->add(new \DateInterval('PT' . $minutesToEarliestNextCall . 'M'));
+            $call->setEarliestNextCall($earliestNextCall);
+
+            $event = $stopwatch->stop($apiInfo);
+            $this->logger->info('{callId}: {apiInfo} duration: {duration} memory: {memory}', array(
+                'callId' => $apiCallId,
+                'apiInfo' => $apiInfo,
+                'duration' => $event->getDuration(),
+                'memory' => $event->getMemory()
+            ));
+        }
+    }
+
+    private function isCallStillValid(ApiCall $call)
+    {
+        return $call != null && $call->getCachedUntil() < new \DateTime('now', new \DateTimeZone('UTC'));
+    }
+
+    private function updateApi(ApiCall $call)
+    {
+        $section = $call->getApi()->getSection();
+        switch ($section) {
+            case 'account':
+                $cachedUntil = $this->accountApi($call);
+                break;
+            case 'char':
+                $cachedUntil = $this->charApi($call);
+                break;
+            case 'corp':
+                $cachedUntil = $this->corpApi($call);
+                break;
+            case 'server':
+            case 'eve':
+            case 'map':
+                $cachedUntil = $this->noKeyApi($call);
+                break;
+            default:
+                throw new \InvalidArgumentException('Unsupported section ' . $section);
+        }
+
+        return $cachedUntil;
+    }
+
+    private function noKeyApi(ApiCall $call)
+    {
+        $pheal = $this->phealFactory->createEveOnline();
+        $updateService = $this->getUpdateService($call);
+
+        return $updateService->update($call, $pheal);
+    }
+
+    private function accountApi(ApiCall $call)
+    {
+        $keyId = $call->getOwnerId();
+        $key = $this->entityManager->getRepository('TariochEveapiFetcherBundle:ApiKey')->find($keyId);
+        if ($key->isActive()) {
+            $pheal = $this->phealFactory->createEveOnline($key->getKeyId(), $key->getVcode());
+            $updateService = $this->getUpdateService($call);
+
+            return $updateService->update($call, $key, $pheal);
+        } else {
+            $call->setActive(false);
+
+            return $call->getCachedUntil();
+        }
+    }
+
+    private function charApi(ApiCall $call)
+    {
+        return $call->getCachedUntil();
+    }
+
+    private function corpApi(ApiCall $call)
+    {
+        return $call->getCachedUntil();
+    }
+
+    private function getUpdateService(ApiCall $call)
+    {
+        $api = $call->getApi();
+        return $this->container->get('tarioch.eveapi.' . $api->getSection() . '.' . $api->getName());
+    }
+}
---
+++ Tests/Unit/Component/Worker/EveWorkerTest.php
@@ -0,0 +1,98 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Tests\Unit\Component\Worker;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Tarioch\EveapiFetcherBundle\Command\ScheduleApiJobsCommand;
+use Mockery as m;
+use Doctrine\ORM\EntityManager;
+use Psr\Log\LoggerInterface;
+use Tarioch\EveapiFetcherBundle\Component\Worker\ApiUpdater;
+use Tarioch\EveapiFetcherBundle\Component\Worker\EveWorker;
+use Doctrine\DBAL\Connection;
+
+class EveWorkerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    /**
+     * @var ApiUpdater
+     */
+    private $apiUpdater;
+    /**
+     * @var Connection
+     */
+    private $connection;
+    /**
+     * @var \GearmanJob;
+     */
+    private $job;
+
+    /**
+     * @var EveWorker
+     */
+    private $eveWorker;
+
+    public function testApiUpdateGood()
+    {
+        $apiCallId = 'apiCallId';
+
+        $this->entityManager->shouldReceive('getConnection')
+            ->andReturn($this->connection);
+        $this->connection->shouldReceive('beginTransaction');
+        $this->job->shouldReceive('workload')
+            ->andReturn($apiCallId);
+        $this->apiUpdater->shouldReceive('update')
+            ->with($apiCallId);
+        $this->entityManager->shouldReceive('flush');
+        $this->connection->shouldReceive('commit');
+
+        $this->eveWorker->apiUpdate($this->job);
+    }
+
+    public function testApiUpdateException()
+    {
+        $apiCallId = 'apiCallId';
+        $exception = new \Exception('myexception');
+
+        $this->entityManager->shouldReceive('getConnection')
+            ->andReturn($this->connection);
+        $this->connection->shouldReceive('beginTransaction');
+        $this->job->shouldReceive('workload')
+            ->andReturn($apiCallId);
+        $this->apiUpdater->shouldReceive('update')
+            ->with($apiCallId)
+            ->andThrow($exception);
+        $this->connection->shouldReceive('rollBack');
+        $this->entityManager->shouldReceive('close');
+        $this->logger->shouldReceive('critical')
+            ->with(
+                '{callId}: Unhandled exception',
+                array('callId' => $apiCallId, 'exception' => $exception)
+            );
+
+        try {
+            $this->eveWorker->apiUpdate($this->job);
+            $this->fail('Exception expected');
+        } catch (\Exception $e) {
+            $this->assertEquals($exception->getMessage(), $e->getMessage());
+        }
+    }
+
+    public function setUp()
+    {
+        $this->entityManager = m::mock('Doctrine\ORM\EntityManager');
+        $this->logger = m::mock('Psr\Log\LoggerInterface');
+        $this->apiUpdater = m::mock('Tarioch\EveapiFetcherBundle\Component\Worker\ApiUpdater');
+        $this->connection = m::mock('Doctrine\DBAL\Connection');
+        $this->job = m::mock('\GearmanJob');
+
+        $this->eveWorker = new EveWorker($this->entityManager, $this->logger, $this->apiUpdater);
+    }
+}
