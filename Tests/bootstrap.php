800440a55f4bb4959594eaaf6b92c20091b77aa2
Patrick Ruckstuhl <patrick@tario.org>
2013-12-11 13:29:46 +0100

    improved check for autoload file

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,9 +2,11 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $directVendors = is_file($loaderFile = __DIR__.'/../vendor/autoload.php');
-$transitiveVendors = is_file($loaderFile = __DIR__.'/../../../../../autoload.php');
-if (!$directVendors && !$transitiveVendors) {
-    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+if (!$directVendors) {
+    $transitiveVendors = is_file($loaderFile = __DIR__.'/../../../../../autoload.php');
+    if (!$transitiveVendors) {
+        throw new \LogicException('Could not find autoload.php in vendor Did you run "composer install --dev"?');
+    }
 }
 
 $loader = require $loaderFile;
