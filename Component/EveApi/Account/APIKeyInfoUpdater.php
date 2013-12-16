f359b0a3e28cf7aac342ed4493b41db7d7f8a3a9
Patrick Ruckstuhl <patrick@ch.tario.org>
2013-12-16 23:22:07 +0100

    very rough draft for algorithm to update the current possible api calls

--- Component/EveApi/Account/APIKeyInfoUpdater.php
+++ Component/EveApi/Account/APIKeyInfoUpdater.php
@@ -5,6 +5,7 @@ use JMS\DiExtraBundle\Annotation as DI;
 use Doctrine\ORM\EntityManager;
 use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
 use Pheal\Pheal;
+use Pheal\Core\Element;
 use Tarioch\EveapiFetcherBundle\Component\EveApi\KeyApi;
 use Tarioch\EveapiFetcherBundle\Entity\ApiKey;
 use Tarioch\EveapiFetcherBundle\Entity\AccountAPIKeyInfo;
@@ -37,13 +38,7 @@ class APIKeyInfoUpdater implements KeyApi
         $api = $pheal->accountScope->APIKeyInfo();
         $apiKey = $api->key;
 
-        // update api key info
-        $entity = $this->loadOrCreate($key);
-        $entity->setAccessMask($apiKey->accessMask);
-        if (! empty($apiKey->expires)) {
-            $entity->setExpires(new \DateTime($apiKey->expires));
-        }
-        $entity->setType($apiKey->type);
+        $this->updateApiKeyInfo($key, $apiKey);
 
         // update associated characters
         $repository = $this->entityManager->getRepository('TariochEveapiFetcherBundle:AccountCharacter');
@@ -52,6 +47,9 @@ class APIKeyInfoUpdater implements KeyApi
         foreach ($characterEntities as $characterEntity) {
             $charEntityMap[$characterEntity->getCharacterId()] = $characterEntity;
         }
+
+        $chars = array();
+        $corps = array();
         foreach ($apiKey->characters as $char) {
             $charId = $char->characterID;
             if (isset($charEntityMap[$charId])) {
@@ -61,9 +59,13 @@ class APIKeyInfoUpdater implements KeyApi
                 $charEntity = new AccountCharacter($key, $charId);
                 $this->entityManager->persist($charEntity);
             }
+            $chars[] = $charId;
 
             $charEntity->setCharacterName($char->characterName);
-            $charEntity->setCorporationId($char->corporationID);
+
+            $corpId = $char->corporationID;
+            $charEntity->setCorporationId($corpId);
+            $corps[] = $corpId;
             $charEntity->setCorporationName($char->corporationName);
         }
 
@@ -72,7 +74,75 @@ class APIKeyInfoUpdater implements KeyApi
             $this->entityManager->remove($characterEntity);
         }
 
+        $this->updateApiCalls($key, $apiKey->accessMask, $chars, $corps);
+
         return $api->cached_until;
+    }
+
+    private function updateApiCalls(ApiKey $key, $accessMask, array $chars, array $corps)
+    {
+        $apiRepo = $this->entityManager->getRepository('TariochEveapiFetcherBundle:Api');
+        $apiCallRepo = $this->entityManager->getRepository('TariochEveapiFetcherBundle:ApiCall');
+
+
+        $currentApiCallMap = array();
+        $currentApiCalls = $apiCallRepo->findByKey($key);
+        foreach ($currentApiCalls as $apiCall) {
+            $apiId = $apiCall->getApi()->getApiId();
+            if (!isset($currentApiCallMap[$apiId])) {
+                $currentApiCallMap[$apiId] = array();
+            }
+
+            $currentApiCallMap[$apiId][$apiCall->getOwnerId()] = $apiCall;
+        }
+
+        $validApis = $apiRepo->loadValidApis($accessMask);
+        foreach ($validApis as $api) {
+            $section = $api->getSection();
+            $owners;
+            if ($section === 'account') {
+                $owners = $key->getKeyId();
+            } elseif ($section === 'char') {
+                $owners = $chars;
+            } elseif ($section === 'corp') {
+                $owners = $corps;
+            }
+
+            $apiId = $api->getApiId();
+            if (isset($currentApiCallMap[$apiId])) {
+                $currentOwners = array_keys($currentApiCallMap[$apiId]);
+                $addedOwners = array_diff($owners, $currentOwners);
+                foreach ($addedOwners as $owner) {
+                    $this->entityManager->persist(new ApiCall($api, $owner));
+                }
+
+                $removedOwners = array_diff($currentOwners, $owners);
+                foreach ($removedOwners as $owner) {
+                    $this->entityManager->remove($currentApiCallMap[$apiId][$owner]);
+                }
+                unset($currentApiCallMap[$apiId]);
+            } else {
+                foreach ($owners as $owner) {
+                    $this->entityManager->persist(new ApiCall($api, $owner));
+                }
+            }
+        }
+
+        foreach ($currentApiCallMap as $apis) {
+            foreach ($apis as $calls) {
+                $this->entityManager->remove($calls);
+            }
+        }
+    }
+
+    private function updateApiKeyInfo(ApiKey $key, Element $apiKey)
+    {
+        $entity = $this->loadOrCreate($key);
+        $entity->setAccessMask($apiKey->accessMask);
+        if (! empty($apiKey->expires)) {
+            $entity->setExpires(new \DateTime($apiKey->expires));
+        }
+        $entity->setType($apiKey->type);
     }
 
     private function loadOrCreate(ApiKey $key)
