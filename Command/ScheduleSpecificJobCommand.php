309b9c3914c70a628ef4161bafdf3ea9b7acc03f
Patrick Ruckstuhl <patrick@ch.tario.org>
2013-12-30 01:00:04 +0100

    debug command for scheduling a specifc apicall added

---
+++ Command/ScheduleSpecificJobCommand.php
@@ -0,0 +1,41 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ScheduleSpecificJobCommand extends ContainerAwareCommand
+{
+    /**
+     * @inheritdoc
+     */
+    protected function configure()
+    {
+        $this->setName('eve:api:debug:scheduleSpecificJob')
+            ->setDescription('Schedule a specific api command for fetching')
+            ->addArgument('callId', InputArgument::REQUIRED, 'ApiCallId');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $gearman = $this->getContainer()->get('gearman');
+        $entityManager = $this->getContainer()->get('doctrine.orm.eveapi_entity_manager');
+
+        $callId = $input->getArgument('callId');
+        $call = $entityManager->find('TariochEveapiFetcherBundle:ApiCall', $callId);
+        $api = $call->getApi();
+
+        $worker = $api->getWorker();
+        $job = $worker . '~apiUpdate';
+        $callId = $call->getApiCallId();
+
+        $gearman->doBackgroundJob($job, $callId, $callId);
+        $output->writeln('Scheduled ' . $callId . ': ' . $worker . ' ' . $api->getSection() . $api->getName());
+    }
+}
