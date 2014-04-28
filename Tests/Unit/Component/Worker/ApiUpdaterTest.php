78a546802cea98ecaf0d65637d6d1e714f6ac086
Patrick Ruckstuhl <patrick@ch.tario.org>
2014-04-28 11:48:12 +0200

    updated test

--- Tests/Unit/Component/Worker/ApiUpdaterTest.php
+++ Tests/Unit/Component/Worker/ApiUpdaterTest.php
@@ -104,7 +104,7 @@ class ApiUpdaterTest extends \PHPUnit_Framework_TestCase
 
         $this->logger->shouldReceive('error');
         $this->apiCall->shouldReceive('increaseErrorCount');
-        $this->apiCall->shouldReceive('getErrorCount')->andReturn(6);
+        $this->apiCall->shouldReceive('getErrorCount')->andReturn(21);
         $this->apiCall->shouldReceive('setActive')->with(false)->once();
 
         $this->mockFinishApiCall();
