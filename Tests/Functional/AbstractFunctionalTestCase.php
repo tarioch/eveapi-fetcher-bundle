d2c1c6dad65095871ae5ce9df86eccb196a7ee3d
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-04-27 22:26:01 +0200

    start of functional api tests

--- Tests/Functional/AbstractFunctionalTestCase.php
+++ Tests/Functional/AbstractFunctionalTestCase.php
@@ -2,10 +2,17 @@
 namespace Tarioch\EveapiFetcherBundle\Tests\Functional;
 
 use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Pheal\Core\Config;
+use Pheal\Cache\ForcedFileStorage;
 
 class AbstractFunctionalTestCase extends WebTestCase
 {
     protected $entityManager;
+
+	protected function get($dependency)
+	{
+		return $this->getContainer()->get($dependency);
+	}
 
     public function setUp()
     {
@@ -13,7 +20,10 @@ class AbstractFunctionalTestCase extends WebTestCase
         $this->runCommand('doctrine:database:create', array('--connection' => 'eveapi', '--no-interaction' => true));
         $out = $this->runCommand('doctrine:migrations:migrate', array('--em' => 'eveapi', '--no-interaction' => true));
         $this->assertContains('++ migrated', $out, 'Doctrine Migrations Failed');
-        $this->entityManager = $this->getContainer()->get('doctrine.orm.eveapi_entity_manager');
+        $this->entityManager = $this->get('doctrine.orm.eveapi_entity_manager');
+        
+        $phealConfig = Config::getInstance();
+        $phealConfig->cache = new ForcedFileStorage(__DIR__ . '/../Api');
     }
 
     public function tearDown()
---
+++ Tests/Functional/Api/NoKey/EveRefTypesTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Tests\Functional\Api\NoKey;
+
+use Tarioch\EveapiFetcherBundle\Tests\Functional\AbstractFunctionalTestCase;
+use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
+
+class EveRefTypesTest extends AbstractFunctionalTestCase
+{
+	private $api;
+	private $pheal;
+	
+    public function testLoadEveRefTypes()
+    {
+    	$api->update(new ApiCall("dummy"), $this->pheal);
+    }
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->api = $this->get('tarioch.eveapi.eve.RefTypes');
+        $phealFactory = $this->get('tarioch.pheal.factory');
+        $this->pheal = $phealFactory->createEveOnline();
+    }
+}
