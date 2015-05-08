c3a0271fc7702fc6a860eb273c487944e8f1a6db
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-05-09 00:51:26 +0200

    migrations

---
+++ DoctrineMigrations/Version20150508004812.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add corp blueprint
+ */
+class Version20150508004812 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql("INSERT INTO `api` VALUES(28, 128, 'TariochEveapiFetcherEveWorker', 'corp', 'IndustryJobsHistory', 360);");
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+    }
+}
