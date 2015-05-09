a0c369971a730967f715c1c4ed42694f784e862f
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-05-09 21:12:04 +0200

    fixed migration

--- DoctrineMigrations/Version20150508004812.php
+++ DoctrineMigrations/Version20150508004812.php
@@ -20,7 +20,7 @@ class Version20150508004812 extends AbstractMigration
         $this->addSql("INSERT INTO `api` VALUES(28, 128, 'TariochEveapiFetcherEveWorker', 'corp', 'IndustryJobsHistory', 360);");
         $this->addSql("DROP INDEX job_owner ON corpIndustryJob;");
         $this->addSql("ALTER TABLE corpIndustryJob ADD ownerID BIGINT UNSIGNED NOT NULL, CHANGE installerName installerName VARCHAR(255) NOT NULL, CHANGE solarSystemName solarSystemName VARCHAR(255) NOT NULL, CHANGE blueprintTypeName blueprintTypeName VARCHAR(255) NOT NULL, CHANGE productTypeName productTypeName VARCHAR(255) NOT NULL;");
-        $this->addSql("CREATE UNIQUE INDEX job_owner ON corpIndustryJob (jobId, ownerId;");
+        $this->addSql("CREATE UNIQUE INDEX job_owner ON corpIndustryJob (jobId, ownerId);");
     }
 
     /**
