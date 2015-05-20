e50d199e5ec6a004e2bfc19c44a4c9038a39d241
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-05-20 22:00:31 +0200

    customs offices updater

---
+++ Component/EveApi/Corp/CustomsOfficeUpdater.php
@@ -0,0 +1,57 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Component\EveApi\Corp;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
+use Pheal\Pheal;
+use Tarioch\EveapiFetcherBundle\Entity\ApiKey;
+use Tarioch\EveapiFetcherBundle\Entity\CorpCustomsOffice;
+
+/**
+ * @DI\Service("tarioch.eveapi.corp.CustomsOffices")
+ */
+class CustomsOfficeUpdater extends AbstractCorpUpdater
+{
+    /**
+     * @inheritdoc
+     */
+    public function update(ApiCall $call, ApiKey $key, Pheal $pheal)
+    {
+        $owner = $call->getOwner();
+        $corpId = $owner->getCorporationId();
+        $api = $pheal->corpScope->customsOffices();
+
+        $this->entityManager
+            ->createQuery('delete from TariochEveapiFetcherBundle:CorpCustomsOffice c where c.ownerId=:ownerId')
+            ->setParameter('ownerId', $corpId)
+            ->execute();
+
+        $this->addCustomsOffices($api->pocos, $corpId);
+
+        return $api->cached_until;
+    }
+
+    private function addCustomsOffices($customOffices, $corpId)
+    {
+        foreach ($customOffices as $customOffice) {
+            $entity = new CorpCustomsOffice();
+            $entity->setOwnerId($corpId);
+            $entity->setItemId($customOffice->itemID);
+            $entity->setSolarSystemId($customOffice->solarSystemID);
+            $entity->setSolarSystemName($customOffice->solarSystemName);
+            $entity->setReinforceHour($customOffice->reinforceHour);
+            $entity->setAllowAlliance($customOffice->allowAlliance);
+            $entity->setAllowStandings($customOffice->allowStandings);
+            $entity->setStandingLevel($customOffice->standingLevel);
+            $entity->setTaxRateAlliance($customOffice->taxRateAlliance);
+            $entity->setTaxRateCorp($customOffice->taxRateCorp);
+            $entity->setTaxRateStandingHigh($customOffice->taxRateStandingHigh);
+            $entity->setTaxRateStandingGood($customOffice->taxRateStandingGood);
+            $entity->setTaxRateStandingNeutral($customOffice->taxRateStandingNeutral);
+            $entity->setTaxRateStandingBad($customOffice->taxRateStandingBad);
+            $entity->setTaxRateStandingHorrible($customOffice->taxRateStandingHorrible);
+
+            $this->entityManager->persist($entity);
+        }
+    }
+}
---
+++ Tests/Functional/Api/Corp/CustomsOfficeTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Tarioch\EveapiFetcherBundle\Tests\Functional\Api\Corp;
+
+use Tarioch\EveapiFetcherBundle\Tests\Functional\AbstractFunctionalTestCase;
+use Tarioch\EveapiFetcherBundle\Entity\ApiCall;
+use Tarioch\EveapiFetcherBundle\Entity\ApiKey;
+use Tarioch\EveapiFetcherBundle\Entity\AccountCharacter;
+use Tarioch\EveapiFetcherBundle\Component\EveApi\Corp\CustomsOfficeUpdater;
+use Pheal\Pheal;
+
+class CustomsOfficeTest extends AbstractFunctionalTestCase
+{
+    private $api;
+    private $pheal;
+
+    public function testUpdate()
+    {
+        $key = new ApiKey(123, 'dummyvcode');
+        $owner = new AccountCharacter($key, 123);
+        $owner->setCorporationId(11);
+        $call = new ApiCall('dummyapi', $owner, $key);
+        $this->api->update($call, $key, $this->pheal);
+        $this->entityManager->flush();
+        $repo = $this->entityManager->getRepository('TariochEveapiFetcherBundle:CorpCustomsOffice');
+        $customsOffice = $repo->findOneByItemId(42);
+        $this->assertEquals(11, $customsOffice->getSolarSystemId());
+        $this->assertEquals('SolarSystemName', $customsOffice->getSolarSystemName());
+        $this->assertEquals(22, $customsOffice->getReinforceHour());
+        $this->assertEquals(true, $customsOffice->getAllowAlliance());
+        $this->assertEquals(true, $customsOffice->getAllowStandings());
+        $this->assertEquals(-2, $customsOffice->getStandingLevel());
+        $this->assertEquals(0.01, $customsOffice->getTaxRateAlliance());
+        $this->assertEquals(0.02, $customsOffice->getTaxRateCorp());
+        $this->assertEquals(0.03, $customsOffice->getTaxRateStandingHigh());
+        $this->assertEquals(0.04, $customsOffice->getTaxRateStandingGood());
+        $this->assertEquals(0.05, $customsOffice->getTaxRateStandingNeutral());
+        $this->assertEquals(0.06, $customsOffice->getTaxRateStandingBad());
+        $this->assertEquals(0.07, $customsOffice->getTaxRateStandingHorrible());
+    }
+    
+    public function setUp()
+    {
+        parent::setUp();
+        $this->api = new CustomsOfficeUpdater($this->entityManager);
+        $this->pheal = new Pheal(123, 'dummyvcode');
+    }
+}
