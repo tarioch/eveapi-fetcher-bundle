9c4d05294ae2a32aa6a952ec74e6ae53c9d01201
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-05-20 21:26:17 +0200

    generate getters/setters

--- Entity/CorpCustomsOffice.php
+++ Entity/CorpCustomsOffice.php
@@ -79,12 +79,344 @@ class CorpCustomsOffice
     private $taxRateStandingNeutral;
 
     /**
-     * @ORM\Column(name="taxRateStandingNeutral", type="decimal", precision=17, scale=2)
+     * @ORM\Column(name="taxRateStandingBad", type="decimal", precision=17, scale=2)
      */
-    private $taxRateStandingNeutral;
+    private $taxRateStandingBad;
 
     /**
      * @ORM\Column(name="taxRateStandingHorrible", type="decimal", precision=17, scale=2)
      */
     private $taxRateStandingHorrible;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set ownerId
+     *
+     * @param integer $ownerId
+     * @return CorpCustomsOffice
+     */
+    public function setOwnerId($ownerId)
+    {
+        $this->ownerId = $ownerId;
+    
+        return $this;
+    }
+
+    /**
+     * Get ownerId
+     *
+     * @return integer 
+     */
+    public function getOwnerId()
+    {
+        return $this->ownerId;
+    }
+
+    /**
+     * Set itemId
+     *
+     * @param integer $itemId
+     * @return CorpCustomsOffice
+     */
+    public function setItemId($itemId)
+    {
+        $this->itemId = $itemId;
+    
+        return $this;
+    }
+
+    /**
+     * Get itemId
+     *
+     * @return integer 
+     */
+    public function getItemId()
+    {
+        return $this->itemId;
+    }
+
+    /**
+     * Set solarSystemId
+     *
+     * @param integer $solarSystemId
+     * @return CorpCustomsOffice
+     */
+    public function setSolarSystemId($solarSystemId)
+    {
+        $this->solarSystemId = $solarSystemId;
+    
+        return $this;
+    }
+
+    /**
+     * Get solarSystemId
+     *
+     * @return integer 
+     */
+    public function getSolarSystemId()
+    {
+        return $this->solarSystemId;
+    }
+
+    /**
+     * Set reinforceHour
+     *
+     * @param integer $reinforceHour
+     * @return CorpCustomsOffice
+     */
+    public function setReinforceHour($reinforceHour)
+    {
+        $this->reinforceHour = $reinforceHour;
+    
+        return $this;
+    }
+
+    /**
+     * Get reinforceHour
+     *
+     * @return integer 
+     */
+    public function getReinforceHour()
+    {
+        return $this->reinforceHour;
+    }
+
+    /**
+     * Set allowAlliance
+     *
+     * @param boolean $allowAlliance
+     * @return CorpCustomsOffice
+     */
+    public function setAllowAlliance($allowAlliance)
+    {
+        $this->allowAlliance = $allowAlliance;
+    
+        return $this;
+    }
+
+    /**
+     * Get allowAlliance
+     *
+     * @return boolean 
+     */
+    public function getAllowAlliance()
+    {
+        return $this->allowAlliance;
+    }
+
+    /**
+     * Set allowStandings
+     *
+     * @param boolean $allowStandings
+     * @return CorpCustomsOffice
+     */
+    public function setAllowStandings($allowStandings)
+    {
+        $this->allowStandings = $allowStandings;
+    
+        return $this;
+    }
+
+    /**
+     * Get allowStandings
+     *
+     * @return boolean 
+     */
+    public function getAllowStandings()
+    {
+        return $this->allowStandings;
+    }
+
+    /**
+     * Set standingLevel
+     *
+     * @param string $standingLevel
+     * @return CorpCustomsOffice
+     */
+    public function setStandingLevel($standingLevel)
+    {
+        $this->standingLevel = $standingLevel;
+    
+        return $this;
+    }
+
+    /**
+     * Get standingLevel
+     *
+     * @return string 
+     */
+    public function getStandingLevel()
+    {
+        return $this->standingLevel;
+    }
+
+    /**
+     * Set taxRateAlliance
+     *
+     * @param string $taxRateAlliance
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateAlliance($taxRateAlliance)
+    {
+        $this->taxRateAlliance = $taxRateAlliance;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateAlliance
+     *
+     * @return string 
+     */
+    public function getTaxRateAlliance()
+    {
+        return $this->taxRateAlliance;
+    }
+
+    /**
+     * Set taxRateCorp
+     *
+     * @param string $taxRateCorp
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateCorp($taxRateCorp)
+    {
+        $this->taxRateCorp = $taxRateCorp;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateCorp
+     *
+     * @return string 
+     */
+    public function getTaxRateCorp()
+    {
+        return $this->taxRateCorp;
+    }
+
+    /**
+     * Set taxRateStandingHigh
+     *
+     * @param string $taxRateStandingHigh
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateStandingHigh($taxRateStandingHigh)
+    {
+        $this->taxRateStandingHigh = $taxRateStandingHigh;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateStandingHigh
+     *
+     * @return string 
+     */
+    public function getTaxRateStandingHigh()
+    {
+        return $this->taxRateStandingHigh;
+    }
+
+    /**
+     * Set taxRateStandingGood
+     *
+     * @param string $taxRateStandingGood
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateStandingGood($taxRateStandingGood)
+    {
+        $this->taxRateStandingGood = $taxRateStandingGood;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateStandingGood
+     *
+     * @return string 
+     */
+    public function getTaxRateStandingGood()
+    {
+        return $this->taxRateStandingGood;
+    }
+
+    /**
+     * Set taxRateStandingNeutral
+     *
+     * @param string $taxRateStandingNeutral
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateStandingNeutral($taxRateStandingNeutral)
+    {
+        $this->taxRateStandingNeutral = $taxRateStandingNeutral;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateStandingNeutral
+     *
+     * @return string 
+     */
+    public function getTaxRateStandingNeutral()
+    {
+        return $this->taxRateStandingNeutral;
+    }
+
+    /**
+     * Set taxRateStandingBad
+     *
+     * @param string $taxRateStandingBad
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateStandingBad($taxRateStandingBad)
+    {
+        $this->taxRateStandingBad = $taxRateStandingBad;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateStandingBad
+     *
+     * @return string 
+     */
+    public function getTaxRateStandingBad()
+    {
+        return $this->taxRateStandingBad;
+    }
+
+    /**
+     * Set taxRateStandingHorrible
+     *
+     * @param string $taxRateStandingHorrible
+     * @return CorpCustomsOffice
+     */
+    public function setTaxRateStandingHorrible($taxRateStandingHorrible)
+    {
+        $this->taxRateStandingHorrible = $taxRateStandingHorrible;
+    
+        return $this;
+    }
+
+    /**
+     * Get taxRateStandingHorrible
+     *
+     * @return string 
+     */
+    public function getTaxRateStandingHorrible()
+    {
+        return $this->taxRateStandingHorrible;
+    }
 }
