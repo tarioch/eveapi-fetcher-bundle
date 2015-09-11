e83351a848b6bed164d52bde8d6f6042c80739ac
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-09-11 23:11:52 +0200

    added lookup methods to find a key by characterId/corporationId

--- Entity/ApiKeyRepository.php
+++ Entity/ApiKeyRepository.php
@@ -15,4 +15,34 @@ class ApiKeyRepository extends EntityRepository
 
         return $q->getResult();
     }
+    
+    public function readKeyForCorp($corpId)
+    {
+        $qb = $this->createQueryBuilder('ak');
+        $q = $qb->select('ak')
+            ->join('Tarioch\EveapiFetcherBundle\Entity\AccountAPIKeyInfo', 'aki', 'WITH', '1=1')
+            ->join('Tarioch\EveapiFetcherBundle\Entity\AccountCharacter', 'ac', 'WITH', '1=1')
+            ->where('aki.key = ak AND aki.type = :keyType AND ac.key = ak AND ac.corporationId = :corpId')
+            ->getQuery()
+            ->setMaxResults(1)
+            ->setParameter('keyType', 'Corporation')
+            ->setParameter('corpId', $corpId);
+            
+        return $q->getOneOrNullResult();
+    }
+
+    public function readKeyForChar($charId)
+    {
+        $qb = $this->createQueryBuilder('ak');
+        $q = $qb->select('ak')
+            ->join('Tarioch\EveapiFetcherBundle\Entity\AccountAPIKeyInfo', 'aki', 'WITH', '1=1')
+            ->join('Tarioch\EveapiFetcherBundle\Entity\AccountCharacter', 'ac', 'WITH', '1=1')
+            ->where('aki.key = ak AND aki.type IN(:keyTypes) AND ac.key = ak AND ac.characterId = :charId')
+            ->getQuery()
+            ->setMaxResults(1)
+            ->setParameter('keyTypes', array('Account', 'Character'))
+            ->setParameter('charId', $charId);
+
+        return $q->getOneOrNullResult();
+    }
 }
